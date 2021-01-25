17852854a25e65be4bf0772f606a9bbe85a4a1f9
Max Marrone <max@marrone.nyc>
2021-01-25 08:33:10 -0500

    Experiment with reading a file.

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 // Modules to control application life and create native browser window
-const {app, BrowserWindow} = require('electron')
+const {app, dialog, BrowserWindow} = require('electron')
 const path = require('path')
 
 function createWindow () {
@@ -18,6 +18,7 @@ function createWindow () {
 
   // Open the DevTools.
   // mainWindow.webContents.openDevTools()
+  
 }
 
 // This method will be called when Electron has finished
--- renderer.js
+++ renderer.js
@@ -1,6 +1,42 @@
+"use strict";
+
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // No Node.js APIs are available in this process because
 // `nodeIntegration` is turned off. Use `preload.js` to
 // selectively enable features needed in the rendering
 // process.
+
+function parseLogEntry(logEntryString) {
+	try {
+		return JSON.parse(logEntryString);
+	}
+	catch (e) {
+		if (e instanceof SyntaxError) {
+			return "<invalid JSON>";
+		}
+		else {
+			throw e;
+		}
+	}
+}
+
+async function parseLog(blob) {
+	const text = await blob.text();
+	
+	// TODO: These can block the rendering thread.
+	// Parsing a 133 MB log dump takes about 2 sec in total.
+	const unparsedLogEntries = text.split("\n");
+	const logEntries = unparsedLogEntries.map(parseLogEntry);
+	return logEntries;
+}
+
+const fileInput = document.getElementById('file');
+fileInput.addEventListener('input', event => {
+	// TODO: This should be cancelable somehow.
+	console.log("oninput");
+	const file = event.target.files[0];
+	parseLog(file).then((logEntries) => {
+		console.log(logEntries);
+	});
+});
