3444f0d8406f58bf82fefbca9edcf288db7316c1
Max Marrone <max@marrone.nyc>
2021-01-26 10:51:11 -0500

    Add some comments.

--- renderer.js
+++ renderer.js
@@ -1,5 +1,11 @@
 "use strict";
 
+// Next up:
+// - Iterate through all log lines, sort into boots
+// 
+
+
+
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // No Node.js APIs are available in this process because
@@ -13,6 +19,9 @@ function parseLogEntry(logEntryString) {
 	}
 	catch (e) {
 		if (e instanceof SyntaxError) {
+			// TODO: It's weird that this would happen.
+			// In my sample log, the very last line is missing its closing '}'.
+			// I'm not sure why that is.
 			return "<invalid JSON>";
 		}
 		else {
@@ -23,7 +32,6 @@ function parseLogEntry(logEntryString) {
 
 async function parseLog(blob) {
 	const text = await blob.text();
-	
 	// TODO: These can block the rendering thread.
 	// Parsing a 133 MB log dump takes about 2 sec in total.
 	const unparsedLogEntries = text.split("\n");
